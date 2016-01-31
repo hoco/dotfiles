4a2bb1b4c0cc088ec7b6a8c0efa0ca28d3a9e57d
hoco <bugman1220@gmail.com>
2016-01-31 20:20:52 +0900

    Ignore ctags file

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)
-files=(.vim .vimrc .zshrc .zprofile .gvimrc .tmux.conf .zprofile .pryrc .tigrc)
+files=(.vim .vimrc .zshrc .zprofile .gvimrc .tmux.conf .zprofile .pryrc .tigrc .gitignore_global)
 
 for dotfile in ${files[@]} 
 do
